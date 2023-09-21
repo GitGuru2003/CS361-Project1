e2bcf98505d2896525f47ccfd6225f32fce7eeb5
Jake Lammers <jakelammers@u.boisestate.edu>
2023-09-21 14:43:34 -0600

    added hashmap implementation to DFAState

--- dfa/DFAState.java
+++ dfa/DFAState.java
@@ -1,8 +1,14 @@
 package fa.dfa;
 import fa.State;
+import java.util.HashMap;
 
 public class DFAState extends State {
+    private HashMap<Character,DFAState> transitionList;
     public DFAState(String name) {
         this.name = name;
+        transitionList = new HashMap<Character,DFAState>();
+    }
+    public DFAState() {
+        //super(name);
     }
 }
